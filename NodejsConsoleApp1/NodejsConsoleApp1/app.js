4fbb76c3726bae8ce062680b72a89cafb0fa3272
Daniel Li <neolee11@yahoo.com>
2014-07-09 19:34:41 -0400

    commit

--- NodejsConsoleApp1/NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/NodejsConsoleApp1/app.js
@@ -1,1 +1,17 @@
-﻿console.log('Hello world');
+﻿Function.prototype.method = function(name, func) {
+    
+}
+
+
+var obj = { prop1 : 'something', prop2 : 'orange' }
+
+var func = function() {
+    return 'result of function';
+}
+
+var arr = [1, 2, 3];
+
+console.log(obj.prop2);
+console.log(func());
+
+console.log(arr[1]);
